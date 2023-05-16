e42270fcda98ffd7703ad5ffa69e873367f012df
Yairtzabar <yairt@fintek.co.il>
2023-05-16 17:17:29 +0300

    aaa

--- app/[lng]/page.jsx
+++ app/[lng]/page.jsx
@@ -3,21 +3,41 @@ import Terms from '@components/Terms/Terms';
 import CustomSelect from '@components/CustomSelect/CustomSelect'
 import { useTranslation } from '@app/i18n';
 import Login from '@components/Login/Login';
+import TwoRadio from '@components/TwoRadio/TwoRadio';
 
 export default async function Home({ params: { lng } }) {
   const { t } = await useTranslation(lng, 'translation')
 
 
   return (
-    <div>
-      {/* <CustomSelect
+    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+      {/* <div>      <CustomSelect
         lng={lng}
         placeholder={t('select')}
-        options={options}
-      /> */}
-      <Login
+        withArrow={false}
+        withNoHeader={false}
+      // options={options}
+      /></div>
+      <div>      <CustomSelect
+        lng={lng}
+        placeholder={t('select')}
+        withNoHeader={true}
+
+      // options={options}
+      /></div>
+      <div>
+        <TwoRadio />
+      </div>
+      <div>      <CustomSelect
+        lng={lng}
+        placeholder={t('select')}
+        withArrow={true}
+      // options={options}
+      /></div> */}
+
+       <Login
       lng={lng}
-       />
+       /> 
     </div>
   )
 }
