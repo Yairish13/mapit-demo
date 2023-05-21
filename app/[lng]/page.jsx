c2540ba89bcc14a62e283dcd53eae4d944796578
Yairtzabar <yairt@fintek.co.il>
2023-05-21 10:05:06 +0300

    added circular progress bar

--- app/[lng]/page.jsx
+++ app/[lng]/page.jsx
@@ -4,13 +4,20 @@ import CustomSelect from '@components/CustomSelect/CustomSelect'
 import { useTranslation } from '@app/i18n';
 import Login from '@components/Login/Login';
 import Stepper from '@components/Stepper/Stepper';
+import CircleProgress from '@components/CircleProgress/CircleProgress';
 
 export default async function Home({ params: { lng } }) {
   const { t } = await useTranslation(lng, 'translation')
 
 
   return (
-      <Stepper />
+    <div className={styles.aaa}>
+      <CircleProgress
+      // percentage={35}
+      circleWidth="200"
+       />
+
+    </div>
   )
 }
 
