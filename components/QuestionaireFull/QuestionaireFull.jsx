7488a3da701532a180ba8f0541d1a1d0263416fc
Yairtzabar <yairt@fintek.co.il>
2023-05-30 15:53:04 +0300

    full structure

--- components/QuestionaireFull/QuestionaireFull.jsx
+++ components/QuestionaireFull/QuestionaireFull.jsx
@@ -3,6 +3,7 @@ import FifthPart from '@components/QuestionaireParts/FifthPart/FifthPart';
 import FirstPart from '@components/QuestionaireParts/FirstPart/FirstPart';
 import FourthPart from '@components/QuestionaireParts/FourthPart/FourthPart';
 import SecondPart from '@components/QuestionaireParts/SecondPart/SecondPart'
+import SeventhPart from '@components/QuestionaireParts/SeventhPart/SeventhPart';
 import SixthPart from '@components/QuestionaireParts/SixthPart/SixthPart';
 import ThirdPart from '@components/QuestionaireParts/ThirdPart/ThirdPart';
 import { useSelector } from 'react-redux';
@@ -11,13 +12,13 @@ const QuestionaireFull = ({ members, lng }) => {
     const activeStep = useSelector((state) => state.general.activeStep);
     return (
         <div>
-            {/* {activeStep === 1 && (<FirstPart members={members} lng={lng} />)}
+            {activeStep === 1 && (<FirstPart members={members} lng={lng} />)}
             {activeStep === 2 && (<SecondPart lng={lng} />)}
             {activeStep === 3 && (<ThirdPart lng={lng} />)}
             {activeStep === 4 && (<FourthPart lng={lng} />)}
-            {activeStep === 5 && (<FifthPart lng={lng} />)} */}
-            {activeStep === 1 && (<SixthPart lng={lng} />)}
+            {activeStep === 5 && (<FifthPart lng={lng} />)}
             {activeStep === 6 && (<SixthPart lng={lng} />)}
+            {activeStep === 7 && (<SeventhPart lng={lng} />)}
         </div>
     )
 }
