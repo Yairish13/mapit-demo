306b61a5f3ebfeec234cb4c9d5bd51fac0612d19
Yairtzabar <yairt@fintek.co.il>
2023-05-31 11:03:09 +0300

    aaa

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -3,12 +3,12 @@ import styles from './Button.module.css';
 
 const Button = ({ mode, id, onClick, disabled, type, children }) => {
     const handleClick = (e) => {
+        console.log('alaa');
         if (onClick) {
             onClick(id);
         }
     };
     const buttonClass = `${styles[mode]} ${disabled ? 'disabled' : ''}`;
-
     return (
         <button
             id={id}
